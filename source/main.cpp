2dac4556ae480a72e0fa5b57649a6e602526726a
abysphalt <djakkunet@gmail.com>
2021-04-08 13:12:30 +0100

    Add files via upload
    Updated to work with the latest LibNX

--- source/main.cpp
+++ source/main.cpp
@@ -23,43 +23,53 @@ int main(int argc, char* argv[]) {
 	if (!initEgl(nwindowGetDefault())) {
 		return EXIT_FAILURE;
 	}
-	
+
 	romfsInit();
-	
+
 	gladLoadGL();
 	initShaders();
-	
+
 	Font Text("romfs:/fonts/BerlinSans.ttf");
 	Text.SetColor(0.0f, 0.0f, 0.0f);
-	
+
 	Sprite Cursor, Segments[44];
 	Cursor.Init(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 4, 4, "romfs:/shapes/cursor.png");
-	
+
 	for(int i = 0; i < 44; i++) {
 		char SegFile[64];
 		sprintf(SegFile, "romfs:/shapes/%i.png", i+1);
-		
+
 		Segments[i].Init(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 1254/4, 1694/4, SegFile);
 	}
-	
+
 	// For motion input
-	u32 handles[3];
-	hidGetSixAxisSensorHandles(&handles[0], 2, CONTROLLER_PLAYER_1, TYPE_JOYCON_PAIR);
-	hidGetSixAxisSensorHandles(&handles[2], 1, CONTROLLER_PLAYER_1, TYPE_PROCONTROLLER);
-	hidStartSixAxisSensor(handles[0]);
-	hidStartSixAxisSensor(handles[1]);
-	hidStartSixAxisSensor(handles[2]);
-	
+	    // Configure our supported input layout: a single player with standard controller styles
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+
+    // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
+    PadState pad;
+    padInitializeDefault(&pad);
+
+    // It's necessary to initialize these separately as they all have different handle values
+    HidSixAxisSensorHandle handles[4];
+    hidGetSixAxisSensorHandles(&handles[0], 1, HidNpadIdType_Handheld, HidNpadStyleTag_NpadHandheld);
+    hidGetSixAxisSensorHandles(&handles[1], 1, HidNpadIdType_No1,      HidNpadStyleTag_NpadFullKey);
+    hidGetSixAxisSensorHandles(&handles[2], 2, HidNpadIdType_No1,      HidNpadStyleTag_NpadJoyDual);
+    hidStartSixAxisSensor(handles[0]);
+    hidStartSixAxisSensor(handles[1]);
+    hidStartSixAxisSensor(handles[2]);
+    hidStartSixAxisSensor(handles[3]);
+
 	// User Variables
 	int MotionSensitivity = 150;
 	int OrcaMoveSpeed = 400;
 	int Flexibility = 1000;
 	int WhaleColR = 255, WhaleColG = 255, WhaleColB = 255;
 	int BGColR = 255, BGColG = 255, BGColB = 255;
-	
+
 	bool textHidden = false;
 	bool isBehind = false;
-	
+
 	varTable variableTable[] = {
 		{ "Motion Sensitivity: %i", &MotionSensitivity },
 		{ "Whale Move Speed: %i", &OrcaMoveSpeed },
@@ -71,40 +81,43 @@ int main(int argc, char* argv[]) {
 		{ "Background Color G: %i", &BGColG },
 		{ "Background Color B: %i", &BGColB },
 	};
-	
+
 	int varSelection = 0;
-	
+
 	while (appletMainLoop()) {
-		hidScanInput();
-		u32 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
-		u32 kHeld = hidKeysHeld(CONTROLLER_P1_AUTO);
-		if (kDown & KEY_PLUS) {
+		// Scan the gamepad. This should be done once for each frame
+        padUpdate(&pad);
+
+		//hidScanInput();
+		u64 kDown = padGetButtonsDown(&pad); // hidKeysDown(CONTROLLER_P1_AUTO);
+		u64 kHeld = padGetButtonsUp(&pad); // hidKeysHeld(CONTROLLER_P1_AUTO);
+		if (kDown & HidNpadButton_Plus) {
 			break;
 		}
-		
-		else if(kDown & KEY_Y) {
+
+		else if(kDown & HidNpadButton_Y) {
 			Cursor.posX = SCREEN_WIDTH/2;
 			Cursor.posY = SCREEN_HEIGHT/2;
 		}
-		else if(kDown & KEY_X) {
+		else if(kDown & HidNpadButton_X) {
 			isBehind = !isBehind;
 		}
-		else if(kDown & KEY_B) {
+		else if(kDown & HidNpadButton_B) {
 			textHidden = !textHidden;
 		}
-		
-		else if((kDown & KEY_DOWN) && varSelection < (int)(sizeof(variableTable)/sizeof(varTable)) - 1) {
+
+		else if((kDown & HidNpadButton_AnyDown) && varSelection < (int)(sizeof(variableTable)/sizeof(varTable)) - 1) {
 			varSelection++;
 		}
-		else if((kDown & KEY_UP) && varSelection > 0) {
+		else if((kDown & HidNpadButton_AnyUp) && varSelection > 0) {
 			varSelection--;
 		}
-		else if(kDown & KEY_RIGHT) {
+		else if(kDown & HidNpadButton_AnyRight) {
 			if(varSelection == 0) MotionSensitivity += 10;
 			else if(varSelection == 1) OrcaMoveSpeed += 10;
 			else if(varSelection == 2) Flexibility += 10;
 		}
-		else if(kHeld & KEY_RIGHT) {
+		else if(kHeld & HidNpadButton_AnyRight) {
 			if(varSelection == 3 && WhaleColR < 255) WhaleColR++;
 			else if(varSelection == 4 && WhaleColG < 255) WhaleColG++;
 			else if(varSelection == 5 && WhaleColB < 255) WhaleColB++;
@@ -112,12 +125,12 @@ int main(int argc, char* argv[]) {
 			else if(varSelection == 7 && BGColG < 255) BGColG++;
 			else if(varSelection == 8 && BGColB < 255) BGColB++;
 		}
-		else if(kDown & KEY_LEFT) {
+		else if(kDown & HidNpadButton_AnyLeft) {
 			if(varSelection == 0) MotionSensitivity -= 10;
 			else if(varSelection == 1) OrcaMoveSpeed -= 10;
 			else if(varSelection == 2) Flexibility -= 10;
 		}
-		else if(kHeld & KEY_LEFT) {
+		else if(kHeld & HidNpadButton_AnyLeft) {
 			if(varSelection == 3 && WhaleColR > 0) WhaleColR--;
 			else if(varSelection == 4 && WhaleColG > 0) WhaleColG--;
 			else if(varSelection == 5 && WhaleColB > 0) WhaleColB--;
@@ -125,28 +138,51 @@ int main(int argc, char* argv[]) {
 			else if(varSelection == 7 && BGColG > 0) BGColG--;
 			else if(varSelection == 8 && BGColB > 0) BGColB--;
 		}
-		
-		touchPosition touch;
-		
-		SixAxisSensorValues sixaxis;
-		hidSixAxisSensorValuesRead(&sixaxis, CONTROLLER_P1_AUTO, 1);
-		
-		if(hidTouchCount() > 0) {
-			hidTouchRead(&touch, 0);
-			
-			Cursor.posX = touch.px;
-			Cursor.posY = touch.py;
-		}
-		
-		Cursor.posX -= sixaxis.gyroscope.z * MotionSensitivity;
-		Cursor.posY -= sixaxis.gyroscope.x * MotionSensitivity;
-		
+
+
+		HidSixAxisSensorState sixaxis = {0};
+        u64 style_set = padGetStyleSet(&pad);
+if (style_set & HidNpadStyleTag_NpadHandheld)
+            hidGetSixAxisSensorStates(handles[0], &sixaxis, 1);
+        else if (style_set & HidNpadStyleTag_NpadFullKey)
+            hidGetSixAxisSensorStates(handles[1], &sixaxis, 1);
+        else if (style_set & HidNpadStyleTag_NpadJoyDual) {
+            // For JoyDual, read from either the Left or Right Joy-Con depending on which is/are connected
+            u64 attrib = padGetAttributes(&pad);
+            if (attrib & HidNpadAttribute_IsLeftConnected)
+                hidGetSixAxisSensorStates(handles[2], &sixaxis, 1);
+            else if (attrib & HidNpadAttribute_IsRightConnected)
+                hidGetSixAxisSensorStates(handles[3], &sixaxis, 1);
+        }
+
+
+
+    hidInitializeTouchScreen();
+    s32 prev_touchcount=0;
+    HidTouchScreenState state={0};
+        if (hidGetTouchScreenStates(&state, 1))
+		{
+            if (state.count != prev_touchcount)
+            {
+                prev_touchcount = state.count;
+            }
+
+            for(s32 i=0; i<state.count; i++)
+            {
+                Cursor.posX = state.touches[i].x;
+                Cursor.posY = state.touches[i].y;
+            }
+        }
+
+		Cursor.posX -= sixaxis.angular_velocity.z * MotionSensitivity;
+		Cursor.posY -= sixaxis.angular_velocity.x * MotionSensitivity;
+
 		// Cursor boundaries
 		if(Cursor.posX < 0) Cursor.posX = 0;
 		else if(Cursor.posX > SCREEN_WIDTH) Cursor.posX = SCREEN_WIDTH;
 		if(Cursor.posY < 0) Cursor.posY = 0;
 		else if(Cursor.posY > SCREEN_HEIGHT) Cursor.posY = SCREEN_HEIGHT;
-		
+
 		for(int i = 0; i < 44; i++) {
 			if(i != 0) {
 				Segments[i].posX = CosLerp(Segments[i].posX, Segments[i-1].posX, Flexibility);
@@ -156,11 +192,11 @@ int main(int argc, char* argv[]) {
 				Segments[i].posY = CosLerp(Segments[i].posY, Cursor.posY - 100, OrcaMoveSpeed); // Added 100 to align orca with cursor correctly
 			}
 		}
-		
+
 		// Render loop begins here
 		glClearColor(BGColR/255.0f, BGColG/255.0f, BGColB/255.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
+
 		if(!textHidden) {
 			Text.DrawText(10, 10, 0.75f, "Killer Whale Cursor for Switch v1.0");
 			Text.DrawText(10, 50, 0.65f, "Use Joy-Con R or touchscreen to move the whale");
@@ -168,7 +204,7 @@ int main(int argc, char* argv[]) {
 			Text.DrawText(10, 120, 0.65f, "[X] Toggle between front/back view");
 			Text.DrawText(10, 150, 0.65f, "[B] Show/hide on-screen text");
 			Text.DrawText(10, 195, 0.65f, "Use D-pad/sticks to change values");
-			
+
 			for(int i = 0; i < (int)(sizeof(variableTable)/sizeof(varTable)); i++) {
 				if(varSelection == i) {
 					Text.SetColor(1.0f, 0.0f, 0.0f);
@@ -177,7 +213,7 @@ int main(int argc, char* argv[]) {
 				Text.SetColor(0.0f, 0.0f, 0.0f);
 			}
 		}
-		
+
 		if(!isBehind) {
 			for(int i = 0; i < 44; i++) {
 				Segments[i].SetColor(WhaleColR/255.0f, WhaleColG/255.0f, WhaleColB/255.0f);
@@ -190,17 +226,18 @@ int main(int argc, char* argv[]) {
 			}
 		}
 		Cursor.Draw();
-		
+
 		SwapBuffers();
 	}
-	
+
 	hidStopSixAxisSensor(handles[0]);
 	hidStopSixAxisSensor(handles[1]);
 	hidStopSixAxisSensor(handles[2]);
-	
+	hidStopSixAxisSensor(handles[3]);
+
 	deinitShaders();
 	deinitEgl();
-	
+
 	romfsExit();
 	return EXIT_SUCCESS;
 }
